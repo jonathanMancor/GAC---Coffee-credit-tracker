5ac07efd1e44956b92d67276a4011ec3ad5527de
jonathanMancor <77824159+jonathanMancor@users.noreply.github.com>
2023-03-01 13:52:02 -0500

    Add files via upload

---
+++ Models/Order.cs
@@ -0,0 +1,13 @@
+﻿using Coffee_Project.Data;
+
+namespace Coffee_Project.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public DrinkType DrinkType { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ Models/UsersDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Coffee_Project.Models
+{
+    public class UsersDto
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+        public decimal Credit { get; set; }
+    }
+}
